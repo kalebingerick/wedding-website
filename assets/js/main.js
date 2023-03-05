75b893cb4fe10ebde816ce88216aeebd16daab54
Kaleb Davis <kaleb.davis39@gmail.com>
2023-03-05 18:49:29 -0500

    Add mobile navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,4 +44,28 @@
     onscroll(document, headerScrolled)
   }
 
+  /**
+   * Toggle mobile nav
+   */
+  // on('click', '.mobile-nav-toggle', function(e) {
+  //   select('#navbar').classList.toggle('navbar-mobile');
+  //   this.classList.toggle('bi-list');
+  //   this.classList.toggle('bi-x');
+  // });
+  
+  // on('click', '.navbar .dropbown > a', function(e) {
+  //   if (select('#navbar').classList.contains('navbar-mobile')) {
+  //     e.preventDefault();
+  //     this.nextElementSibling.classList.toggle('dropdown-active');
+  //   }
+  // }, true);
+  on('click', '.mobile-nav-toggle', function(e) {
+    var element = select('#mobile-links');
+    if (element.style.display === 'grid') {
+      element.style.display = 'none';
+    } else {
+      element.style.display = 'grid';
+    }
+  });
+
 })();
